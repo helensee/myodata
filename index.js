78cea13d9abb2320bf42213bc23bff67ceebb191
Helen See <helen.see@rutgers.edu>
2016-03-18 11:06:15 -0400

    included myodata.js
    all myo code should be in myodata.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var app = express();
 app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
     res.render('pages/index');
