ab9c1a171882c1f95b99e13d14313caf053ef69b
Jasmine Feng <feng.jasmine54@gmail.com>
2016-03-23 21:00:10 -0400

    reads csv file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var $ = jQuery = require('jQuery');
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -10,3 +12,20 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 app.listen(8080);
+
+
+console.log("CSV starts here!");
+var people = [];
+var fileContents = fs.readFileSync('test.csv');
+var lines = fileContents.toString().split('\n');
+
+for (var i = 0; i < lines.length; i++) {
+    people.push(lines[i].toString().split(','));
+}
+
+for (var i = 0; i < lines.length; i++) {
+    for (var j = 0; j < 3; j++) {
+        console.log(people[i][j]);
+    }
+    console.log('\n');
+}
