87bb316515e8d90e4ed299c59c9d767fa3685892
Jasmine Feng <feng.jasmine54@gmail.com>
2016-03-27 15:43:56 -0400

    db set up, table init, frontend displayed

--- index.js
+++ index.js
@@ -52,23 +52,51 @@ app.get('/', function(request, response) {
 	var fileContents = fs.readFileSync('test.csv');
 	var lines = fileContents.toString().split('\n');
 
-	for (var i = 0; i < lines.length; i++) {
-	    data.push(lines[i].toString().split(','));
-	}
+	db.serialize(function() {		
+		/*
+		* 
+		NOTE: UNCOMMENT ONLY IF YOU WANT TO ADD MORE THINGS TO DB
+		AS IN THE TEST.CSV FILE IS NEW
+		OR THERE WILL BE REPEATS BC I DIDNT CHECK FOR THAT lol
+		----------------------------------
+		for (var i = 1; i < lines.length; i++) {
+	    	data.push(lines[i].toString().split(','));
+		}
 
-	for (var i = 0; i < lines.length; i++) {
-	    for (var j = 0; j < 5; j++) {
-	        //console.log(data[i][j]);
-	    }
-	    //console.log('\n');
-	}
-	response.render('pages/index.ejs', {
-		data: data
+		var stmt = db.prepare("INSERT into myodata(Timestamp,x,y,z,w) VALUES (?,?,?,?,?)");
+		for (var i = 0; i < lines.length; i++) {
+			if(data[i] != null) {
+				console.log(data[i]);
+				stmt.run(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]);
+			}
+	        
+	        //db.run("INSERT into myodata(Timestamp,x,y,z,w) VALUES (?)", data[i]);
+	        //stmt.run(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]);
+	        //stmt.run(d0,d1,d2,d3,d4);
+		}
+		stmt.finalize();
+		console.log('end add to db');
+		*/
+
+
+		db.all("SELECT * FROM myodata", function(err, rows) {  
+		        rows.forEach(function (row) {  
+		            console.log(row.Timestamp);  
+		        });
+		        response.render('pages/index.ejs', {
+					data: rows
+				});
+		    });   
+		db.close();  
+
+		
 	});
+	
+	
 
 });
 
-db.close();
+//db.close();
 
 /*
 //CODE TO READ CSV FILE
